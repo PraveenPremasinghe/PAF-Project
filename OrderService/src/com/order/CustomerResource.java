82f1b70a86f9b2f5a853a947028ef45a438f67a7
Praveen Premasinghe <57926001+PraveenPremasinghe@users.noreply.github.com>
2021-04-25 00:48:00 +0530

    Update Customer and customerservice file

---
+++ OrderService/src/com/order/CustomerResource.java
@@ -0,0 +1,73 @@
+package com.order;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.order.model.Customer;
+
+@Path("/customer")
+
+public class CustomerResource {
+	
+
+	Customer customer = new Customer();
+	    
+	    @GET
+	    @Path("/")
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public String getCustomer(@QueryParam("userid") String userid) {
+	    	if(userid == null) {
+	    	
+	    	
+	        return customer.getCustomer().toString();
+	    	}
+	    	else
+	    	{
+	    		 return customer.getSingleCustomer(Integer.parseInt(userid)).toString();
+	    	}
+	    }
+	    
+	    
+	    @POST
+	    @Path("/")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public String insertCustomer(String customerJsonString) {
+	        JsonObject customerJson =new  JsonParser().parse(customerJsonString).getAsJsonObject();
+	        
+	        return (customer.insertCustomer(customerJson.get("username").getAsString(), customerJson.get("password").getAsString(), customerJson.get("email").getAsString(), customerJson.get("phone").getAsInt())).toString();
+	    }
+	    
+	    @PUT
+	    @Path("/")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public String updateCustomer(String customerJsonString) {
+	        JsonObject customerJson =new  JsonParser().parse(customerJsonString).getAsJsonObject();
+	        
+	        return (customer.updateCustomer( customerJson.get("userid").getAsInt() ,customerJson.get("username").getAsString(), customerJson.get("password").getAsString(), customerJson.get("email").getAsString(), customerJson.get("phone").getAsInt()  )).toString();
+	       
+	    }
+	    
+	    
+	    @DELETE
+	    @Path("/")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public String deleteCustomer(@QueryParam("userid") String userid) {
+	        
+	        
+	        return (customer.deleteCustomer(Integer.parseInt(userid))).toString();
+	        
+	    }
+
+}
--- OrderService/src/com/order/model/Customer.java
+++ OrderService/src/com/order/model/Customer.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 public class Customer extends DBConnector {
 
 	public boolean validateCustomer(String username, String password) {
@@ -47,5 +50,253 @@ public class Customer extends DBConnector {
 			return false;
 		}
 		
+	}
+	
+	
+	///////////////////////////////////////Customer logim&signup curd////////////////////////////////////////////////
+
+
+	//Read Customer details
+	public JsonObject getCustomer() {
+		JsonObject result = null;
+		try
+		{
+			Connection con = connect();
+			if (con == null)
+			{
+				result = new JsonObject();
+				result.addProperty("ERROR", "Error while connecting to the database for reading."); 
+			}
+			// create a prepared statement
+			String query = " select * from customer_table";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			ResultSet rs = preparedStmt.executeQuery();
+
+			JsonArray resultArray = new JsonArray();
+			while(rs.next()) {
+				JsonObject customer_table_row = new JsonObject();
+
+				customer_table_row.addProperty("userid", rs.getInt("userid") );
+				customer_table_row.addProperty("username", rs.getString("username") );
+				customer_table_row.addProperty("password", rs.getString("password") );
+				customer_table_row.addProperty("email", rs.getString("email") );
+				customer_table_row.addProperty("phone", rs.getInt("phone") );
+
+				resultArray.add(customer_table_row);
+			}
+			result = new JsonObject();
+			result.add("customer", resultArray);
+
+			con.close();
+		}
+		catch (Exception e)
+		{
+			result = new JsonObject();
+			result.addProperty("EXCEPTION", "Error occured while reading customer");
+			System.err.println(e.getMessage());
+		}
+		return result;
+	}
+
+
+
+	public JsonObject insertCustomer( String username, String password,String email,int phone )
+	{
+		JsonObject result = null;
+		try
+		{
+			Connection con = connect();
+			if (con == null)
+			{
+				result = new JsonObject();
+				result.addProperty("ERROR", "Error while connecting to the database for inserting.");
+			}
+			// create a prepared statement
+			String query = " insert into customer_table"+
+					"(`username`,`password`,`email`,`phone`,)"
+					+ " values (?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			preparedStmt.setString(1, username);
+			preparedStmt.setString(2, password);
+			preparedStmt.setString(3, email);
+			preparedStmt.setInt(4, phone);
+
+			
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			result = new JsonObject();
+			result.addProperty("SUCCESSFUL", "Inserted successfully");
+		}
+		catch (Exception e)
+		{
+			result = new JsonObject();
+			result.addProperty("EXCEPTION","Error while inserting the item.");
+			System.err.println(e.getMessage());
+		}
+		return result;
 	} 
+
+	//update customer
+	public JsonObject updateCustomer( int userid ,String username, String password,String email,int phone)
+	{
+		JsonObject result = null;
+		try
+		{
+			Connection con = connect();
+			if (con == null)
+			{
+				result = new JsonObject();
+				result.addProperty("ERROR", "Error while connecting to the database for updating.");
+			}
+			// create a prepared statement
+			String query = " update customer_table set `username`= ? ,`password` = ?,`email` = ?,`phone` = ? where `userid` = ?";
+PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			preparedStmt.setString(1, username);
+			preparedStmt.setString(2, password);
+			preparedStmt.setString(3, email);
+			preparedStmt.setInt(4, phone);
+			preparedStmt.setInt(5, userid);
+			
+			// execute the statement
+			preparedStmt.execute();
+			
+
+			int status =  preparedStmt.executeUpdate();
+
+			con.close();
+
+			result = new JsonObject();
+
+			if(status > 0 ) {
+
+
+				result.addProperty("SUCCESSFUL", "Updated Successfully");
+
+			}
+			else {
+				result.addProperty("UNSUCCESSFUL", "Updating Failed");	
+			}
+
+		}
+		catch (Exception e)
+		{
+			result = new JsonObject();
+			result.addProperty("EXCEPTION","Error while updating the item.");
+			System.err.println(e.getMessage());
+		}
+		return result;
+	} 
+
+	//delete customer
+	public JsonObject deleteCustomer( int userid )
+	{
+		JsonObject result = null;
+		try
+		{
+			Connection con = connect();
+
+			if (con == null)
+			{
+				result = new JsonObject();
+				result.addProperty("ERROR", "Error while connecting to the database for deleting.");
+			}
+
+
+			// create a prepared statement
+			String query = "delete from customer_table where userid=?";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+
+			// binding values
+			preparedStmt.setInt(1, userid);
+
+			// execute the statement
+			int status =  preparedStmt.executeUpdate();
+
+			con.close();
+
+			result = new JsonObject();
+
+			if(status > 0 ) {
+
+
+				result.addProperty("SUCCESSFUL", "Deleted Successfully");
+
+			}
+			else {
+				result.addProperty("UNSUCCESSFUL", "Deletion Failed");	
+			}
+
+
+		}
+		catch (Exception e)
+		{
+			result = new JsonObject();
+			result.addProperty("EXCEPTION","Error while deleting the item.");
+			System.err.println(e.getMessage());
+		}
+		return result;
+	} 
+
+
+	//get single record
+	public JsonObject getSingleCustomer(int userid) {
+		JsonObject result = null;
+		try
+		{
+			Connection con = connect();
+
+			if (con == null)
+			{
+				result = new JsonObject();
+				result.addProperty("ERROR", "Error while connecting to the database for reading single item."); 
+			}
+
+
+			// create a prepared statement
+			String query = " select * from customer_table where userid=?";
+
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			preparedStmt.setInt(1, userid);
+			ResultSet rs = preparedStmt.executeQuery();
+
+			JsonArray resultArray = new JsonArray();
+			while(rs.next()) {
+				JsonObject customer_table_row = new JsonObject();
+				
+				customer_table_row.addProperty("userid", rs.getInt("userid") );
+				customer_table_row.addProperty("username", rs.getString("username") );
+				customer_table_row.addProperty("password", rs.getString("password") );
+				customer_table_row.addProperty("email", rs.getString("email") );
+				customer_table_row.addProperty("phone", rs.getInt("phone") );
+			
+				resultArray.add(customer_table_row);
+				
+			}
+			result = new JsonObject();
+			result.add("customer", resultArray);
+
+			con.close();
+		}
+		catch (Exception e)
+		{
+			result = new JsonObject();
+			result.addProperty("EXCEPTION", "Error occured while reading Customer");
+			System.err.println(e.getMessage());
+		}
+		return result;
+	}
+
+///////////////////////////////////////Customer logim&signup curd////////////////////////////////////////////////
+
+	
 }
